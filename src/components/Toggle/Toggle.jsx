9afd16b5c49d9c5ee38c6538e090504e68136454
Harsh <harshkesharwani53@gmail.com>
2025-02-24 11:45:15 +0530

    Adding toggling component

---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,34 @@
+
+import React, { useEffect, useRef, useState } from "react";
+
+import "./Toggle.css";
+ import Moon from "../../assets/Moon.svg";
+import Sun from "../../assets/Sun.svg";
+
+const Toggle = ({ children }) => {
+  
+   const [dark, setDark] = useState(false); // Use boolean for dark mode
+ 
+   const darktheme = () => {
+     setDark(!dark);
+   };
+ 
+   useEffect(() => {
+     document.body.className = dark ?  "dark-theme" : "light-theme";
+   }, [dark]);
+
+  return (
+      <div className="toggle-container">
+        <div className="dl_dec">
+            <a href="#home" className=" dl_pos " >
+              <img onClick={darktheme} src={dark ?  Sun : Moon} alt="" className="moon "  />
+            </a>
+        </div>
+        
+        {children}
+      </div>
+  
+  );
+};
+
+export default Toggle;
