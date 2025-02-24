ffce75969a80654f7c601b60f5099b66609d901d
Harsh <harshkesharwani53@gmail.com>
2025-02-24 11:49:01 +0530

    Adding portfolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,38 @@
+
+import Work1 from "../../assets/Work1.png";
+import Work2 from "../../assets/Work2.png";
+import Work3 from "../../assets/work3.png";
+
+const Menu = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Ninja-AI ",
+    category: "Fullstack",
+    linkl:"https://ninja-ai.vercel.app/",
+    linkg:"https://github.com/harsh0045/Ninja-AI"
+    
+  },
+  
+  {
+    id: 2,
+    image: Work2,
+    title: "Airbnb-Clone",
+    category: "Clone",
+    linkl:"https://abnb-clone-cyan.vercel.app/",
+    linkg:"https://github.com/harsh0045/Airbnb-Clone"
+    
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Second-Brain" ,
+    category: "Fullstack",
+    linkl:"https://second-brain-frontend-three.vercel.app/",
+    linkg:"https://github.com/harsh0045/Second-Brain"
+  }
+
+  
+  ];
+
+export default Menu
